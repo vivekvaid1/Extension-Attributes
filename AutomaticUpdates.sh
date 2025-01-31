4e142efecb15bb587d855802155fea73ebf71858
vivekvaid1 <vivekvaid1@icloud.com>
2025-01-31 20:57:17 +0530

    new

---
+++ AutomaticUpdates.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+UpdateStatus=`defaults read /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload`
+if [[ "$UpdateStatus" = "0" ]]; then
+    result="Disabled"
+elif [[ "$UpdateStatus" = "1"  ]]; then
+    result="Enabled"
+else 
+    result="Error"
+fi
+echo "<result>$result</result>"
